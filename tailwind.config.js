6cd4f53dff992c6631ca5b9fbf65fce1cba3c9f3
kai-commits <kaimeikle123@gmail.com>
2022-08-08 14:58:13 -0700

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
+  theme: {
+    extend: {
+      colors: {
+        'blackeye-blue': '#121629',
+        darkblue: '#232946',
+        lightblue: '#b8c1ec',
+        offwhite: '#fffffe',
+        lightpink: '#eebbc3',
+      },
+    },
+  },
   plugins: [],
-}
+};
