a4bdca4d9996ae486682dce287a9b2bf1376f641
kai-commits <kaimeikle123@gmail.com>
2022-08-08 10:39:27 -0700

    project init

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Crynance</title>
+        <meta name='crypto finance tracker' content='' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
