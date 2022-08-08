b4a40e5f62b31e33b9ef329279349f54d95ea1bc
kai-commits <kaimeikle123@gmail.com>
2022-08-08 14:58:21 -0700

    init nav bar

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+export const Header = (): JSX.Element => {
+  return (
+    <div className='flex flex-col items-center'>
+      <div className='text-lightpink text-3xl font-bold cursor-pointer p-4'>
+        Crynance
+      </div>
+      <div className='text-offwhite text-5xl'>
+        $500
+      </div>
+      <div className='text-lightblue p-4'>
+        $400
+      </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { Header } from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className=''>
+    <div className='min-h-screen bg-darkblue'>
       <Head>
         <title>Crynance</title>
         <meta name='crypto finance tracker' content='' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className='container max-w-5xl mx-auto'>
+        <Header />
       </div>
     </div>
   );
