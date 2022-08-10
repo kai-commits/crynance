ca2996eaa609038932301d37b64e537736840737
kai-commits <kaimeikle123@gmail.com>
2022-08-10 15:01:31 -0700

    add api data

--- components/Coin.tsx
+++ components/Coin.tsx
@@ -1,13 +1,11 @@
+import Image from 'next/image';
 import Link from 'next/link';
-import { Smile } from 'react-feather';
 
 interface Coin {
   name: string;
   symbol: string;
   logo: string;
   currentMarketValue: number;
-  amountOwned: number;
-  ownedValue: number;
 }
 
 export const Coin = ({
@@ -15,23 +13,23 @@ export const Coin = ({
   symbol,
   logo,
   currentMarketValue,
-  amountOwned,
-  ownedValue,
 }: Coin): JSX.Element => {
   return (
     <Link href={`/bought/${name}`}>
       <div className='w-full bg-offwhite max-w-3xl py-2 mt-3 rounded cursor-pointer'>
         <div className='flex justify-between items-center'>
           <div className='flex items-center mx-1'>
-            <Smile className='mx-2 h-9 w-9' />
+            <div className='flex px-2 items-center'>
+              <Image src={logo} width={35} height={35} alt={symbol} />
+            </div>
             <div className='flex flex-col font-medium'>
               <div className='text-lg text-blackeye-blue'>{name}</div>
               <div className='text-sm text-darkblue'>${currentMarketValue}</div>
             </div>
           </div>
           <div className='flex flex-col items-end mx-3 font-medium'>
-            <div className='text-lg'>{amountOwned}</div>
-            <div className='text-sm text-darkblue'>${ownedValue}</div>
+            <div className='text-lg'>10</div>
+            <div className='text-sm text-darkblue'>$100</div>
           </div>
         </div>
       </div>
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,25 +1,34 @@
-import { Coin } from "./Coin";
+import axios from 'axios';
+import useSWR from 'swr';
+import { Coin } from './Coin';
+
+interface ParsedMarkets {
+  id: string;
+  symbol: string;
+  name: string;
+  logo: string;
+  currentMarketValue: number;
+  priceChangePercentage: number;
+}
+
+const axiosFetcher = async (url: string) => {
+  return await axios.get(url).then((res) => res.data);
+};
 
 export const Main = (): JSX.Element => {
+  const { data: marketsResponse } = useSWR('/api/markets', axiosFetcher);
+  console.log(marketsResponse);
   return (
     <div className='flex items-center flex-col bg-lightblue px-5 h-max pb-3'>
-      <Coin name='Bitcoin' marketValue={29800} ownedValue={1000} amountOwned={0.01} />
-      <Coin name='Ethereum' marketValue={2230} ownedValue={3000} amountOwned={1.5} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
-      <Coin name='Solana' marketValue={50} ownedValue={500} amountOwned={10} />
+      {marketsResponse?.map((coin: ParsedMarkets) => (
+        <Coin
+          key={coin.id}
+          name={coin.name}
+          symbol={coin.symbol}
+          logo={coin.logo}
+          currentMarketValue={coin.currentMarketValue}
+        />
+      ))}
     </div>
   );
 };
