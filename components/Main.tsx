33887d0fb603be8317e5c5d9c00221b7dc58fa52
kai-commits <kaimeikle123@gmail.com>
2022-08-11 11:49:28 -0700

    wip buy page

--- components/Main.tsx
+++ components/Main.tsx
@@ -19,7 +19,7 @@ export const Main = (): JSX.Element => {
   const { data: marketsResponse } = useSWR('/api/markets', axiosFetcher);
   console.log(marketsResponse);
   return (
-    <div className='flex items-center flex-col bg-lightblue px-5 h-max pb-3'>
+    <div className='flex items-center flex-col bg-lightblue px-5 h-max pb-3 min-h-screen'>
       {marketsResponse?.map((coin: ParsedMarkets) => (
         <Coin
           key={coin.id}
