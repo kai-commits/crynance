63171848b6ee60d0f2adf9df6b72dbc07a236cf0
kai-commits <kaimeikle123@gmail.com>
2022-09-06 16:45:35 -0700

    wip dynamic price percentage change

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -11,6 +11,7 @@ import { axiosFetcher } from '../helpers/axiosFetcher';
 interface TimeRangeButton {
   name: string;
   queryString: string;
+  percentageChange: number;
 }
 
 const { today, oneDayAgo, oneWeekAgo, oneMonthAgo, oneYearAgo } = {
@@ -21,52 +22,75 @@ const { today, oneDayAgo, oneWeekAgo, oneMonthAgo, oneYearAgo } = {
   oneYearAgo: dayjs().subtract(1, 'year').unix(),
 };
 
-const timeRangeButtons: TimeRangeButton[] = [
-  {
-    name: '1D',
-    queryString: `?from=${oneDayAgo}&to=${today}`,
-  },
-  {
-    name: '1W',
-    queryString: `?from=${oneWeekAgo}&to=${today}`,
-  },
-  {
-    name: '1M',
-    queryString: `?from=${oneMonthAgo}&to=${today}`,
-  },
-  {
-    name: '1Y',
-    queryString: `?from=${oneYearAgo}&to=${today}`,
-  },
-];
-
 const CoinPage: NextPage = () => {
   const { pid } = useRouter().query;
   const { data: coinDataResponse } = useSWR(`/api/coins/${pid}`, axiosFetcher);
+  const timeRangeButtons: TimeRangeButton[] = [
+    {
+      name: '1D',
+      queryString: `?from=${oneDayAgo}&to=${today}`,
+      percentageChange:
+        Math.round(coinDataResponse?.priceChangePercentage1D * 100) / 100,
+    },
+    {
+      name: '1W',
+      queryString: `?from=${oneWeekAgo}&to=${today}`,
+      percentageChange:
+        Math.round(coinDataResponse?.priceChangePercentage1W * 100) / 100,
+    },
+    {
+      name: '1M',
+      queryString: `?from=${oneMonthAgo}&to=${today}`,
+      percentageChange:
+        Math.round(coinDataResponse?.priceChangePercentage1M * 100) / 100,
+    },
+    {
+      name: '1Y',
+      queryString: `?from=${oneYearAgo}&to=${today}`,
+      percentageChange:
+        Math.round(coinDataResponse?.priceChangePercentage1Y * 100) / 100,
+    },
+  ];
 
   const [activeTimeButton, setActiveTimeButton] = useState('1D');
+  const [percentageChange, setPercentageChange] = useState(0);
   const [queryParams, setQueryParams] = useState(
     timeRangeButtons[0].queryString
   );
 
   const timeRangeButtonClasses = (button: TimeRangeButton): string => {
     return classNames('text-bold border-2 px-2 rounded', {
-      'border-darkblue': activeTimeButton === button.name,
-      'border-lightblue': activeTimeButton !== button.name,
+      'border-darkblue text-lightblue': activeTimeButton !== button.name,
+      'border-offwhite text-offwhite': activeTimeButton === button.name,
     });
   };
 
   const timeButtonHandler = (button: TimeRangeButton) => {
     setActiveTimeButton(button.name);
     setQueryParams(button.queryString);
+    setPercentageChange(button.percentageChange);
   };
 
   return (
     <div className='flex flex-col justify-between h-screen'>
-      <div className='flex grow bg-lightblue'>
+      <div className='flex grow bg-darkblue'>
         <div className='flex flex-col mx-auto w-full max-w-3xl'>
-          <div className='flex justify-center text-darkblue text-3xl font-bold cursor-pointer p-4'>
-            {pid}
+          <div className='flex flex-col items-center'>
+            <div className='text-lightpink text-3xl font-bold cursor-pointer p-4'>
+              {coinDataResponse?.name}
+            </div>
+            <div className='flex justify-center'>
+              <div className='text-offwhite pr-1'>
+                $
+                {Math.round(coinDataResponse?.currentMarketValue * 100000) /
+                  100000}
+              </div>
+              {percentageChange >= 0 ? (
+                <div className='text-green-500 pl-1'>{percentageChange}%</div>
+              ) : (
+                <div className='text-red-500 pl-1'>{percentageChange}%</div>
+              )}
+            </div>
           </div>
           {pid && <LineChart queryParams={queryParams} pid={pid} />}
 
