17c92bcfb5ed99067ea723219bc8676fb83239f2
kai-commits <kaimeikle123@gmail.com>
2022-09-15 16:43:08 -0700

    remove unnecessary imports

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -2,21 +2,11 @@ import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { LineChart } from '../components/graphs/LineChart';
 import { Nav } from '../components/Nav';
-import {
-  Dispatch,
-  JSXElementConstructor,
-  ReactElement,
-  ReactFragment,
-  ReactPortal,
-  SetStateAction,
-  useEffect,
-  useState,
-} from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import classNames from 'classnames';
 import dayjs from 'dayjs';
 import useSWR from 'swr';
 import { ParsedCoin } from './api/coins/[pid]';
-import Link from 'next/link';
 import { Transaction } from '../components/Transaction';
 import axios from 'axios';
 import { roundNumber } from '../helpers/math';
@@ -44,7 +34,7 @@ const axiosFetcher = async (
   return await axios.get(`${url}`).then((res) => setter(res.data));
 };
 
-const CoinPage: NextPage = () => {
+const CoinPage: NextPage = (): JSX.Element => {
   const { pid } = useRouter().query;
   const [coinDataResponse, setCoinDataResponse] = useState<ParsedCoin>();
   const [activeTimeButton, setActiveTimeButton] = useState<string>('1D');
@@ -125,108 +115,111 @@ const CoinPage: NextPage = () => {
 
   if (!coinDataResponse) {
     return <div className='bg-darkblue w-full h-screen'></div>;
-  }
-  return (
-    <>
-      <div className='w-full h-full bg-blackeye-blue'>
-        <div className='flex flex-col justify-between bg-darkblue h-max max-w-3xl mx-auto'>
-          <ConditionWrapper
-            condition={modalOpen}
-            wrapper={(children) => (
-              <div className='bg-black opacity-60 w-full h-full z-50'>
-                {children}
-              </div>
-            )}
-          >
-            <div className='flex flex-col items-center px-5'>
-              <div className='w-full max-w-3xl'>
-                <div className='flex justify-center text-lightpink text-3xl font-bold cursor-pointer p-4'>
-                  {coinDataResponse.name}
+  } else {
+    return (
+      <>
+        <div className='w-full h-full bg-blackeye-blue'>
+          <div className='flex flex-col justify-between bg-darkblue h-max max-w-3xl mx-auto'>
+            <ConditionWrapper
+              condition={modalOpen}
+              wrapper={(children) => (
+                <div className='bg-black opacity-60 w-full h-full z-50'>
+                  {children}
                 </div>
-                <div className='flex justify-evenly text-xl'>
-                  <div className='text-offwhite pr-3'>
-                    ${roundNumber(coinDataResponse.currentMarketValue, 5)}
+              )}
+            >
+              <div className='flex flex-col items-center px-5'>
+                <div className='w-full max-w-3xl'>
+                  <div className='flex justify-center text-lightpink text-3xl font-bold cursor-pointer p-4'>
+                    {coinDataResponse.name}
                   </div>
-                  {percentageChange >= 0 ? (
-                    <div className='text-green-500 pl-3'>
-                      {percentageChange}%
+                  <div className='flex justify-evenly text-xl'>
+                    <div className='text-offwhite pr-3'>
+                      ${roundNumber(coinDataResponse.currentMarketValue, 5)}
                     </div>
-                  ) : (
-                    <div className='text-red-500 pl-3'>{percentageChange}%</div>
-                  )}
+                    {percentageChange >= 0 ? (
+                      <div className='text-green-500 pl-3'>
+                        {percentageChange}%
+                      </div>
+                    ) : (
+                      <div className='text-red-500 pl-3'>
+                        {percentageChange}%
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='w-full mx-auto max-w-3xl'>
-              {pid && <LineChart queryParams={queryParams} pid={pid} />}
-            </div>
-            <div className='flex flex-col items-center px-5'>
-              <div className='w-full max-w-3xl'>
-                <div className='flex justify-between'>
-                  {timeRangeButtons.map((button, index) => (
+              <div className='w-full mx-auto max-w-3xl'>
+                {pid && <LineChart queryParams={queryParams} pid={pid} />}
+              </div>
+              <div className='flex flex-col items-center px-5'>
+                <div className='w-full max-w-3xl'>
+                  <div className='flex justify-between'>
+                    {timeRangeButtons.map((button, index) => (
+                      <button
+                        key={index}
+                        className={timeRangeButtonClasses(button)}
+                        onClick={() => timeButtonHandler(button)}
+                      >
+                        {button.name}
+                      </button>
+                    ))}
+                  </div>
+                  <div className='flex flex-col'>
+                    <div className='flex justify-center text-lightpink text-xl my-5'>
+                      0.146 {coinDataResponse.symbol.toUpperCase()}
+                    </div>
+                    <div className='flex justify-between text-offwhite'>
+                      <div>Current Balance</div>
+                      <div>$500</div>
+                    </div>
+                    <div className='flex justify-between text-lightblue'>
+                      <div>Starting Balance</div>
+                      <div>$400</div>
+                    </div>
+                  </div>
+                  <div className='flex justify-between w-full my-8'>
                     <button
-                      key={index}
-                      className={timeRangeButtonClasses(button)}
-                      onClick={() => timeButtonHandler(button)}
+                      onClick={() => {
+                        setModalActive(true);
+                        setModalOpen(!modalOpen);
+                      }}
+                      className='bg-lightpink px-4 py-2 rounded font-bold text-darkblue cursor-pointer flex-1 mr-5'
                     >
-                      {button.name}
+                      Buy
                     </button>
-                  ))}
-                </div>
-                <div className='flex flex-col'>
-                  <div className='flex justify-center text-lightpink text-xl my-5'>
-                    0.146 {coinDataResponse.symbol.toUpperCase()}
-                  </div>
-                  <div className='flex justify-between text-offwhite'>
-                    <div>Current Balance</div>
-                    <div>$500</div>
-                  </div>
-                  <div className='flex justify-between text-lightblue'>
-                    <div>Starting Balance</div>
-                    <div>$400</div>
-                  </div>
-                </div>
-                <div className='flex justify-between w-full my-8'>
-                  <button
-                    onClick={() => {
-                      setModalActive(true);
-                      setModalOpen(!modalOpen);
-                    }}
-                    className='bg-lightpink px-4 py-2 rounded font-bold text-darkblue cursor-pointer flex-1 mr-5'
-                  >
-                    Buy
-                  </button>
 
-                  <button className='bg-lightpink px-4 py-2 rounded font-bold text-darkblue cursor-pointer flex-1 ml-5'>
-                    Sell
-                  </button>
+                    <button className='bg-lightpink px-4 py-2 rounded font-bold text-darkblue cursor-pointer flex-1 ml-5'>
+                      Sell
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='flex flex-col grow bg-lightblue'>
-              <div className='flex flex-col items-center px-5 pb-3'>
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
+              <div className='flex flex-col grow bg-lightblue'>
+                <div className='flex flex-col items-center px-5 pb-3'>
+                  <Transaction />
+                  <Transaction />
+                  <Transaction />
+                  <Transaction />
+                  <Transaction />
+                </div>
               </div>
-            </div>
-            <Nav />
-          </ConditionWrapper>
+              <Nav />
+            </ConditionWrapper>
 
-          <CoinModal
-            setModalOpen={setModalOpen}
-            modalActive={modalActive}
-            modalOpen={modalOpen}
-            name={coinDataResponse.name}
-            symbol={coinDataResponse.symbol}
-            currentMarketValue={coinDataResponse.currentMarketValue}
-          />
+            <CoinModal
+              setModalOpen={setModalOpen}
+              modalActive={modalActive}
+              modalOpen={modalOpen}
+              name={coinDataResponse.name}
+              symbol={coinDataResponse.symbol}
+              currentMarketValue={coinDataResponse.currentMarketValue}
+            />
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default CoinPage;
