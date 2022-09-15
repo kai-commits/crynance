134455406ef0cf3d7da57ec3a9200fd9eefca111
kai-commits <kaimeikle123@gmail.com>
2022-09-14 20:55:28 -0700

    add sliding animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,37 @@ module.exports = {
         offwhite: '#eeeeee',
         lightpink: '#eebbc3',
       },
+      animation: {
+        slideUp: 'slideUp 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        slideDown:
+          'slideDown 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both',
+      },
+      keyframes: {
+        slideUp: {
+          '0%': {
+            '-webkit-transform': 'translateY(1000px)',
+            transform: 'translateY(1000px)',
+            opacity: 0,
+          },
+          '100%': {
+            '-webkit-transform': 'translateY(0)',
+            transform: 'translateY(0)',
+            opacity: 1,
+          },
+        },
+        slideDown: {
+          '0%': {
+            '-webkit-transform': 'translateY(0)',
+            transform: 'translateY(0)',
+            opacity: 1,
+          },
+          '100%': {
+            '-webkit-transform': 'translateY(1000px)',
+            transform: 'translateY(1000px)',
+            opacity: 0,
+          },
+        },
+      },
     },
   },
   plugins: [],
