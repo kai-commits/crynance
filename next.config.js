efabdc2191dbf561f8b8846f0b2eefaf0a5c4771
kai-commits <kaimeikle123@gmail.com>
2022-10-11 15:56:39 -0700

    remove firebase hosting

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains:['assets.coingecko.com']
-  }
-}
+    domains: ['assets.coingecko.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
